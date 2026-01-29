d85c4f5234fdd0f090d0affcbcb812ea81b07b4b
= <=>
2026-01-29 13:47:04 +0000

    use mlflow to log models we train from airflow dags

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -17,10 +17,20 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
     y = features[target]
-    with mlflow.start_run():
+    time_str = time.strftime('%Y%m%d-%H%M%S')
+    mlflow.set_experiment(f"Experiment_{time_str}")
+
+    mlflow.sklearn.autolog(log_models=False)
+
+    with mlflow.start_run(run_name=f"train_model_{time_str}") as run:
         # insert autolog here ...
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="sklearn_model",
+            registered_model_name="sklearn_model"
+        )
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
 
